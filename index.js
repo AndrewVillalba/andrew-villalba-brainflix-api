2716a44f064e1edbb36ba5ae4ed9f425e6486dbf
AndrewVillalba <andrewvillalba1@gmail.com>
2023-04-23 13:52:09 -0400

    added images

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const videoRoutes = require('./routes/videos')
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
-app.use('/', videoRoutes)
+app.use('/', videoRoutes);
+app.use('/videos', videoRoutes);
 
 app.get('/', (req, res) => {
     res.send("home")
--- routes/videos.js
+++ routes/videos.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const videos = require('../data/videos.json')
+// let videos = require('../data/videos.json');
+const fs = require ('fs');
+const {v4: uuidv4} = require ('uuid');
+
+
+function videoList() {
+    const videoInfoList = fs.readFileSync('./data/videos.json');
+    const parsedData = JSON.parse(videoInfoList);
+    return parsedData;
+}
 
 router.get('/videos', (req, res) => {
+    videos = videoList();
     res.json(videos);
 });
+
+router.get('/:id', (req, res) => {
+    const videoId = req.params.id;
+    let videos = videoList();
+    const video = videos.find((i) => i.id === videoId);
+
+    if(video) {
+        res.json(video);
+    } else {
+        res.status(404).send('Video was not found');
+    }
+})
 
 module.exports = router;
